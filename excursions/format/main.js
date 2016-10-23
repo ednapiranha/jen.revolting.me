1f9b1ba7e48032d6fcf798d243229ff5f27edfaa
Jen <jfong@mozilla.com>
2016-10-23 14:17:41 -0700

    audio and interaction updates

--- excursions/format/main.js
+++ excursions/format/main.js
@@ -2,12 +2,22 @@
   var width = 70;
   var height = 70;
   var title = document.querySelector('h1');
+  var section = document.querySelector('section');
+  var body = document.body;
 
   function render() {
-    document.body.style.backgroundSize = width + 'vh ' + height + 'vh';
+    body.style.backgroundSize = width + 'vh ' + height + 'vh';
     var now = Date.now();
     width = Math.sin(now) * width;
     height = Math.tan(now) * height;
+
+    if (now % 2 === 0) {
+      section.style.opacity = Math.random();
+    }
+
+    if (now % 10 === 0) {
+      section.style.backgroundSize = Math.floor(Math.random() * 90) + '%';
+    }
 
     if (width < 10) {
       width = 70;
